5bc0832bda0770c3e340148d47186a8a6df2601a
Michael Kargas <optimus6128@yahoo.gr>
2021-11-08 17:18:39 +0000

    Mistake with naked memory allocation

--- src/common/engine_texture.c
+++ src/common/engine_texture.c
@@ -71,20 +71,14 @@ Texture *initFeedbackTexture(int posX, int posY, int width, int height, int buff
 
 Texture *loadTexture(char *path)
 {
-	Texture *tex;
-	CCB *tempCel;
-	int size;
+	CCB *tempCel = LoadCel(path, MEMTYPE_ANY);
 
-	(Texture*)AllocMem(sizeof(Texture), MEMTYPE_ANY);
-
-	tempCel = LoadCel(path, MEMTYPE_ANY);
-
-	tex = initTexture(tempCel->ccb_Width, tempCel->ccb_Height, 16, TEXTURE_TYPE_STATIC, NULL, NULL, 0);
+	Texture *tex = initTexture(tempCel->ccb_Width, tempCel->ccb_Height, 16, TEXTURE_TYPE_STATIC, NULL, NULL, 0);
 		// 16bit is the only bpp CEL type extracted from BMPTo3DOCel for now (which is what I currently use to make CEL files and testing)
 		// In the future, I'll try to deduce this from the CEL bits anyway (I already know how, just too lazy to find out again)
 		// Update: BMPTo3DOCel is shit! It saves right now the same format as BMPTo3DOImage (for VRAM structure to use with SPORT copy) instead of the most common linear CEL bitmap structure
 
-	size = (tex->width * tex->height * tex->bpp) / 8;
+	const int size = (tex->width * tex->height * tex->bpp) / 8;
 
 	memcpy(tex->bitmap, tempCel->ccb_SourcePtr, size);
 
